cef1b6c0db23bba9899d1b8ad86c73295651ac2b
Aidan Harris-Tyrrell <aidanharris-tyrrell@ajudeht.local>
2019-12-19 22:20:29 -0800

    genre fix 2

--- tracks.php
+++ tracks.php
@@ -23,7 +23,7 @@ function getTrackList() {
   $pdo = (new SQLiteConnection())->connect();
   $genre = htmlspecialchars($_GET["genre"]);
 
-    $stmt = $pdo->query('SELECT tracks.Name as Name, tracks.UnitPrice as UnitPrice, tracks.Composer as Artist, albums.Title as Album, genres.Name as Genre FROM tracks, albums, genres WHERE ' . (is_null($genre) ? '' : ('genres.Name = "' . $genre . '" AND')) . ' tracks.AlbumID = albums.AlbumId AND tracks.GenreId = genres.GenreId ');
+    $stmt = $pdo->query('SELECT tracks.Name as Name, tracks.UnitPrice as UnitPrice, tracks.Composer as Artist, albums.Title as Album, genres.Name as Genre FROM tracks, albums, genres WHERE tracks.AlbumID = albums.AlbumId AND tracks.GenreId = genres.GenreId ' . (is_null($genre) ? '' : ('AND genres.Name = "' . $genre . '"')));
 
 
     $tracks = [];
